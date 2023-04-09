d8a1d243678a1fc8630c4ff0ea946a9061dec1c9
javascriptsucks <evix1988@live.cn>
2023-04-08 22:18:25 -0600

    setup graphql to return users and posts

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,35 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
-    include GraphQL::Types::Relay::HasNodeField
-    include GraphQL::Types::Relay::HasNodesField
+    # # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    # include GraphQL::Types::Relay::HasNodeField
+    # include GraphQL::Types::Relay::HasNodesField
+    # users
+    field :users, [Types::UserType], null: false
 
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def users
+      User.all
     end
+
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
+    end
+
+    field :posts, [Types::PostType], null: false
+    def posts
+      Post.all
+    end
+
+    field :post, Types::PostType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def post(id:)
+      Post.find(id)
+    end
+
   end
 end
