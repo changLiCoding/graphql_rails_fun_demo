d1855cf2eaa6326aa67fa95de35660f03453fb7b
javascriptsucks <evix1988@live.cn>
2023-04-12 21:46:59 -0600

    graphql sign up mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,7 +2,7 @@ module Types
   class MutationType < Types::BaseObject
     field :create_user, mutation: Mutations::CreateUser
     field :create_post, mutation: Mutations::CreatePost
-    # field :sig_up, mutation: Mutations::SigUp
+    field :sign_up, mutation: Mutations::SignUp
     field :login, mutation: Mutations::Login
   end
 end
